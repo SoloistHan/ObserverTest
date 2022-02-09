70a4e3e651b738536eb15d3a6ff435b0b5a618c6
Soloist <lineage18135@yahoo.com.tw>
2022-02-09 20:59:10 +0800

    First commit

---
+++ ObserverTest/InterfaceWay.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reactive;
+using System.Text;
+
+namespace ObserverTest
+{
+    public class InterfaceWay
+    {
+        public class DesktopApp : ITempatureMonitorObserver
+        {
+            public string OnTempatureChanged(double tempature)
+            {
+               return $"Desktop App被通知溫度變化了: {tempature}";
+            }
+        }
+        public class MobileApp : ITempatureMonitorObserver
+        {
+            public string OnTempatureChanged(double tempature)
+            {
+                return $"Mobile App被通知溫度變化了: {tempature}";
+            }
+        }
+
+        public interface ITempatureMonitorSubject
+        {
+            void RegisterObserver(ITempatureMonitorObserver observer);
+
+            void UnregisterObserver(ITempatureMonitorObserver observer);
+
+            void NotifyTempature();
+        }
+
+        // ITempatureMonitorObserver.cs
+        public interface ITempatureMonitorObserver
+        {
+            string OnTempatureChanged(double tempature);
+        }
+
+        public class TempatureMonitorSubject : ITempatureMonitorSubject
+        {
+            private double tempature;
+
+            public double Tempature
+            {
+                get { return tempature; }
+                set
+                {
+                    var oldTempature = tempature;
+                    if (oldTempature != value)
+                    {
+                        tempature = value;
+                        NotifyTempature();
+                    }
+                }
+            }
+
+            private List<ITempatureMonitorObserver> observers;
+
+            public TempatureMonitorSubject()
+            {
+                observers = new List<ITempatureMonitorObserver>();
+                Console.WriteLine("開始偵測溫度");
+            }
+
+            public void RegisterObserver(ITempatureMonitorObserver observer)
+            {
+                observers.Add(observer);
+            }
+
+            public void UnregisterObserver(ITempatureMonitorObserver observer)
+            {
+                observers.Remove(observer);
+            }
+
+            public void NotifyTempature()
+            {
+                foreach (var observer in observers)
+                {
+                    observer.OnTempatureChanged(tempature);
+                }
+            }
+        }
+
+      
+
+    }
+
+}
---
+++ ObserverTest/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ObserverTest.InterfaceWay;
+
+namespace ObserverTest
+{
+    public partial class Main : Form
+    {
+        double targetTemp = 10;
+        public Main()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void buttonOBinterface_Click(object sender, EventArgs e)
+        {
+            TempatureMonitorSubject mainOB = new TempatureMonitorSubject();
+            MobileApp mobileApp = new MobileApp();
+            DesktopApp desktopApp = new DesktopApp();
+            mainOB.RegisterObserver(mobileApp);
+            mainOB.RegisterObserver(desktopApp);
+
+            richTextBox1.Clear();
+            targetTemp += 1.1;
+            mainOB.Tempature = targetTemp;
+            richTextBox1.AppendText(mobileApp.OnTempatureChanged(targetTemp) + "\r");
+            richTextBox1.AppendText(desktopApp.OnTempatureChanged(targetTemp) + "\r");
+        }
+    }
+}
